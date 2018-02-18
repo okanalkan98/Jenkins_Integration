e112e7a8b7e2136273e37f65e143f21693a1d04d
okanalkan98 <okanalkan83@gmail.com>
2018-02-18 15:43:49 -0500

    first commit

---
+++ src/test/Selenium_Jenkins_Integration.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Selenium_Jenkins_Integration {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Qkan\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+		driver=new ChromeDriver();	
+		driver.get("http://google.com");
+	}
+	
+	@Test
+	public void test() {
+		WebElement searchBox=driver.findElement(By.name("q"));
+		searchBox.sendKeys("porsche"+Keys.ENTER);
+		Assert.assertTrue(driver.getTitle().contains("porsche"));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}

354a30a21dc7d4c208fb576548a64bf193a86ddc
okanalkan98 <okanalkan83@gmail.com>
2018-02-21 01:35:07 -0500

    adding in testng.xml and test_runner.bat file for jenkins integration.

--- test_runner.bat
+++ test_runner.bat
@@ -1,1 +1,1 @@
-java -cp "C:\Users\Qkan\Desktop\JAVA\Java_Codes\Jenkins_Git_Demo\bin";"C:\Users\Qkan\Documents\selenium dependencies\libraries\*";"C:\Users\Qkan\Documents\selenium dependencies\libraries\libs\*;C:\Program Files\eclipse\plugins\*" org.testng.TestNG testng.xml
+java -cp "C:\Users\Qkan\Desktop\JAVA\Java_Codes\Jenkins_Git_Demo\bin";"C:\Users\Qkan\Documents\selenium dependencies\libraries\*";"C:\Users\Qkan\Documents\selenium dependencies\libraries\libs\*";"C:\Program Files\eclipse\plugins\*" org.testng.TestNG testng.xml
